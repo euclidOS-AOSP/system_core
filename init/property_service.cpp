f063ca99d33d47aaf5b8d604d8f1006972480480
AnierinB <anierin@evolution-x.org>
2024-07-19 17:27:59 +0000

    init: Conditionally spoof ro.product.first_api_level to 32 [2/2]
    Devices that shipped on API 33 or higher need to set
    ro.product.first_api_level to 32 in order to pass integrity.
    
    Set SPOOF_FIRST_API_LEVEL_32 := true in BoardConfig*.mk to enable.
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: HDzungx <hdzungx@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1452,6 +1452,9 @@ static void SetSafetyNetProps() {
     InitPropertySet("vendor.boot.vbmeta.device_state", "locked");
     InitPropertySet("vendor.boot.verifiedbootstate", "green");
     InitPropertySet("oplusboot.verifiedbootstate", "green");
+#ifdef SPOOF_FIRST_API_LEVEL_32
+    InitPropertySet("ro.product.first_api_level", "32");
+#endif
 }
 
 void PropertyInit() {
