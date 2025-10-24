095adb85ad394cc5a866177fec9e40da017c0093
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-10-24 21:21:41 +0800

    [development-support] fixing wrong debug hooks placement
    add exemption to strict properties set on user builds
    
    Change-Id: I54d3195f61f58e21276739c8ed6a85891aac7cbe
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -505,6 +505,19 @@ bool CheckControlPropertyPerms(const std::string& name, const std::string& value
     return CheckMacPerms(control_string_full, target_context_full, source_context.c_str(), cr);
 }
 
+static bool is_exempt(const std::string& name, const std::string& source_context) {
+    static const std::vector<std::string> exemption_list = {
+        "persist.sys.ax_debug_enabled",
+        "logpersistd",
+    };
+
+    return std::any_of(exemption_list.begin(), exemption_list.end(),
+        [&](const std::string& exempt) {
+            return name.find(exempt) != std::string::npos ||
+                   source_context.find(exempt) != std::string::npos;
+        });
+}
+
 // This returns one of the enum of PROP_SUCCESS or PROP_ERROR*.
 uint32_t CheckPermissions(const std::string& name, const std::string& value,
                           const std::string& source_context, const ucred& cr, std::string* error) {
@@ -527,7 +540,8 @@ uint32_t CheckPermissions(const std::string& name, const std::string& value,
     const char* type = nullptr;
     property_info_area->GetPropertyInfo(name.c_str(), &target_context, &type);
 
-    if (!CheckMacPerms(name, target_context, source_context.c_str(), cr)) {
+    if (!is_exempt(name, source_context) &&
+        !CheckMacPerms(name, target_context, source_context.c_str(), cr)) {
         *error = "SELinux permission check failed";
         return PROP_ERROR_PERMISSION_DENIED;
     }
@@ -1222,25 +1236,25 @@ void LoadDebugProperties() {
     uint32_t res;
 
     std::string debug_value = GetProperty(DEBUG_PROP, "1");
+    bool debug_enabled = debug_value == "1";
+    LOG(INFO) << DEBUG_PROP << " = " << debug_value;
 
-    if (debug_value == "1") {
-        const std::pair<const char*, const char*> debug_props[] = {
-            {"service.adb.root", "1"},
-            {"ro.adb.secure", "0"},
-            {"ro.debuggable", "1"},
-            {"ro.force.debuggable", "1"},
-            {"persist.sys.usb.config", "adb"},
-            {"sys.usb.config", "adb"}
-        };
+    const std::pair<const char*, const char*> debug_props[] = {
+        {"service.adb.root", debug_enabled ? "1" : "0"},
+        {"ro.adb.secure", debug_enabled ? "0" : "1"},
+        {"ro.debuggable", debug_enabled ? "1" : "0"},
+        {"ro.force.debuggable", debug_enabled ? "1" : "0"},
+        {"persist.sys.usb.config", debug_enabled ? "adb" : "none"},
+        {"sys.usb.config", debug_enabled ? "adb" : "none"}
+    };
 
-        for (const auto& [name, value] : debug_props) {
-            res = PropertySetNoSocket(name, value, &error);
-            if (res == PROP_SUCCESS) {
-                LOG(INFO) << "Debug property '" << name << "' set to '" << value << "'";
-            } else {
-                LOG(ERROR) << "Failed to set debug property '" << name
-                           << "' to '" << value << "': err=" << res << " (" << error << ")";
-            }
+    for (const auto& [name, value] : debug_props) {
+        res = PropertySetNoSocket(name, value, &error);
+        if (res == PROP_SUCCESS) {
+            LOG(INFO) << "Debug property '" << name << "' set to '" << value << "'";
+        } else {
+            LOG(ERROR) << "Failed to set debug property '" << name
+                       << "' to '" << value << "': err=" << res << " (" << error << ")";
         }
     }
 }
@@ -1416,8 +1430,6 @@ void PropertyLoadBootDefaults() {
     
     LoadReleaseBuildProperties();
     
-    LoadDebugProperties();
-
     // Restore the normal property override security after init extension is executed
     weaken_prop_override_security = false;
 
@@ -1642,6 +1654,13 @@ static void HandleInitSocket() {
                 // Always enable usb adb if device is booted with debug ramdisk.
                 update_sys_usb_config();
             }
+
+            weaken_prop_override_security = true;
+            
+            LoadDebugProperties();
+
+            weaken_prop_override_security = false;
+
             InitPropertySet("ro.persistent_properties.ready", "true");
             persistent_properties_loaded = true;
             break;
