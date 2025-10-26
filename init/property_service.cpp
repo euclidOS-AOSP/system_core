a209e3981c7ed76f0ab53ad745641462580fc173
Pranav Vashi <neobuddy89@gmail.com>
2025-10-26 21:55:29 +0530

    init: Add more properties for spoof
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: HDzungx <hdzungx@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1193,6 +1193,11 @@ static void SetSafetyNetProps() {
         {"vendor.boot.verifiedbootstate", "green"},
         {"oplusboot.verifiedbootstate", "green"},
         {"sys.oem_unlock_allowed", "0"},
+        {"ro.oem_unlock_supported", "0"},
+        {"ro.crypto.state", "encrypted"},
+        {"ro.is_ever_orange", "0"},
+        {"ro.secureboot.devicelock", "1"},
+        {"ro.secureboot.lockstate", "locked"},
         {"init.svc.adbd", "stopped"},
 #ifdef SPOOF_FIRST_API_LEVEL_32
         {"ro.product.first_api_level", "32"}
